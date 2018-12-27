f6f0dd5b293e7fa1b7eba91a645bfe1672dd9e6e
Jochio <bjude430@gmail.com>
2018-12-27 16:26:27 +0100

    change body-parser to express.json()

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
-import express from 'express'
-import bodyParser from 'body-parser';
+import express from 'express';
+// import bodyParser from 'body-parser';
+import landRoute from './srcFolder/routes/landingPage';
 
-
+// App is a new express instance
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// This effectively replaces body-parser see https://stackoverflow.com/questions/47232187/express-json-vs-bodyparser-json
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// express.json() - this is needed to get access to request body
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/', landRoute);
 
 
 const port = process.env.PORT || 5700;
@@ -13,3 +21,5 @@ const port = process.env.PORT || 5700;
 app.listen(port, () => console.log(`Dehawk is live on port ${port}`));
 
 export default app;
+
+// babel will help us compile the code to ES5 that nodejs runtime can understand.
